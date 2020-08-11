b1cb8f07bef0bf657555e5fbf2f38f08a53237ee
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2020-08-11 20:31:56 -0300

    Add P01

---
+++ src/P01.elm
@@ -0,0 +1,13 @@
+module P01 exposing (solve)
+
+solve : List a -> Maybe a
+solve list =
+    case list of
+        [] -> 
+            Nothing
+
+        [x] ->
+            Just x
+
+        _ :: r ->
+            solve r
