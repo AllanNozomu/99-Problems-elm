82c85838c35154d32ee12254e8252bf2a7b612e1
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2020-08-11 20:32:16 -0300

    Add tests

---
+++ tests/R01.elm
@@ -0,0 +1,23 @@
+module R01 exposing (suite)
+
+import Expect
+import Fuzz exposing (int, list, string)
+import Test exposing (Test, describe, fuzz, test)
+
+import P01 exposing (solve)
+
+suite : Test
+suite =
+    describe "P01"
+        [ test "Empty list" <|
+            \_ ->
+                Expect.equal (solve []) Nothing
+
+        , test "One element" <|
+            \_ ->
+                Expect.equal (solve [1]) (Just 1)
+
+        , fuzz string "Many elements" <|
+            \_ ->
+                Expect.equal (solve [1,2,3,4,5,9]) (Just 9)
+        ]
