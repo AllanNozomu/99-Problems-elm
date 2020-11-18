788ad83b6c9010477c3604ee458a12e7f1c92f95
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2020-11-18 09:23:41 -0300

    Separate minHeap, use from new Module

--- src/P50.elm
+++ src/P50.elm
@@ -1,142 +1,7 @@
-module P50 exposing (..)
+module P50 exposing (solve)
 
 import Array exposing (Array)
-import Basics
-import Html exposing (a)
-
-
-type alias CmpFuntion a =
-    a -> a -> Basics.Order
-
-
-type alias MinHeap a =
-    { size : Int
-    , arr : Array a
-    , cmpFunction : CmpFuntion a
-    }
-
-
-minHeapParent : Int -> Int
-minHeapParent n =
-    (n - 1) // 2
-
-
-minHeapLeft : Int -> Int
-minHeapLeft n =
-    n * 2 + 1
-
-
-minHeapRight : Int -> Int
-minHeapRight n =
-    n * 2 + 2
-
-
-heapCompare : Int -> Int -> CmpFuntion a -> Array a -> Bool
-heapCompare i j f arr =
-    case ( Array.get i arr, Array.get j arr ) of
-        ( Just ii, Just jj ) ->
-            f ii jj == Basics.LT
-
-        _ ->
-            False
-
-
-arrSwap : Int -> Int -> Array a -> Array a
-arrSwap i j arr =
-    case ( Array.get i arr, Array.get j arr ) of
-        ( Just ii, Just jj ) ->
-            Array.set i jj arr |> Array.set j ii
-
-        _ ->
-            arr
-
-
-minHeapCreate : (a -> a -> Basics.Order) -> MinHeap a
-minHeapCreate f =
-    { size = 0
-    , arr = Array.empty
-    , cmpFunction = f
-    }
-
-
-minHeapPeek : MinHeap a -> Maybe a
-minHeapPeek h =
-    if h.size == 0 then
-        Nothing
-
-    else
-        Array.get 0 h.arr
-
-
-minHeapInsert : a -> MinHeap a -> MinHeap a
-minHeapInsert value heap =
-    let
-        insertAux : Int -> Array a -> Array a
-        insertAux n arr =
-            if n == 0 then
-                arr
-
-            else
-                let
-                    parent =
-                        minHeapParent n
-                in
-                if heapCompare n parent heap.cmpFunction arr then
-                    insertAux parent (arrSwap n parent arr)
-
-                else
-                    arr
-
-        newArr =
-            if Array.length heap.arr <= heap.size then
-                Array.push value heap.arr |> insertAux heap.size
-
-            else
-                Array.set heap.size value heap.arr |> insertAux heap.size
-    in
-    { heap | size = heap.size + 1, arr = newArr }
-
-
-minHeapRemove : MinHeap a -> MinHeap a
-minHeapRemove heap =
-    let
-        newSize =
-            heap.size - 1
-
-        removeAux : Int -> Array a -> Array a
-        removeAux n arr =
-            let
-                left =
-                    minHeapLeft n
-
-                right =
-                    minHeapRight n
-            in
-            case ( left <= (newSize - 1), right <= (newSize - 1) ) of
-                ( True, True ) ->
-                    if heapCompare left right heap.cmpFunction arr && heapCompare left n heap.cmpFunction arr then
-                        removeAux left (arrSwap left n arr)
-
-                    else if heapCompare right n heap.cmpFunction arr then
-                        removeAux right (arrSwap right n arr)
-
-                    else
-                        arr
-
-                ( True, False ) ->
-                    if heapCompare left n heap.cmpFunction arr then
-                        removeAux left (arrSwap left n arr)
-
-                    else
-                        arr
-
-                _ ->
-                    arr
-
-        newArr =
-            arrSwap 0 newSize heap.arr |> removeAux 0
-    in
-    { heap | size = newSize, arr = newArr }
+import DataStructures.MinHeap as MinHeap
 
 
 type Node
@@ -178,9 +43,9 @@ solve : List ( String, Int ) -> List ( String, String )
 solve l =
     let
         heap =
-            List.map createNode l |> List.foldl (\n acc -> minHeapInsert n acc) (minHeapCreate cmpHuffman) |> solveAux
+            List.map createNode l |> List.foldl (\n acc -> MinHeap.insert n acc) (MinHeap.create cmpHuffman) |> solveAux
     in
-    case minHeapPeek heap of
+    case MinHeap.peek heap of
         Just hd ->
             hd.nodes
 
@@ -188,9 +53,9 @@ solve l =
             []
 
 
-solveAux : MinHeap HuffmanData -> MinHeap HuffmanData
+solveAux : MinHeap.MinHeap HuffmanData -> MinHeap.MinHeap HuffmanData
 solveAux heap =
-    case ( minHeapPeek heap, minHeapRemove heap |> minHeapPeek ) of
+    case ( MinHeap.peek heap, MinHeap.remove heap |> MinHeap.peek ) of
         ( Just newLeft, Just newRight ) ->
             let
                 newNodes =
@@ -210,7 +75,7 @@ solveAux heap =
                 newNode =
                     createNode ( newLeft.c ++ newRight.c, newLeft.qty + newRight.qty )
             in
-            solveAux (minHeapRemove heap |> minHeapRemove |> minHeapInsert { newNode | nodes = newNodes })
+            solveAux (MinHeap.remove heap |> MinHeap.remove |> MinHeap.insert { newNode | nodes = newNodes })
 
         _ ->
             heap
