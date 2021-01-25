daec249eb62c556d5224486653b2b3d5a2f75fe8
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2021-01-24 21:55:06 -0300

    Improve P58 using half of complexity

--- src/P58.elm
+++ src/P58.elm
@@ -1,10 +1,21 @@
 module P58 exposing (solve)
 
 import P55 exposing (Tree(..))
-import P56
 
+reverse : Tree comparable -> Tree comparable
+reverse tree =
+    case tree of
+    Empty -> Empty
+    Branch x l r -> Branch x (reverse r) (reverse l)
+
+addReverse : comparable -> Tree comparable -> Tree comparable
+addReverse val tree =
+    Branch val (reverse tree) tree
 
 solve : comparable -> Int -> List (Tree comparable)
 solve val n =
-    P55.solve val n
-        |> List.filter P56.solve
+    if Basics.modBy 2 n == 0 then
+        []
+    else
+        P55.solve val (n // 2) |>
+        List.map (addReverse val)
