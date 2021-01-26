120517c582ad79d22766a53fbf34c0a2be810d95
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2021-01-26 08:16:11 -0300

    elm format

--- src/P58.elm
+++ src/P58.elm
@@ -2,20 +2,27 @@ module P58 exposing (solve)
 
 import P55 exposing (Tree(..))
 
+
 reverse : Tree comparable -> Tree comparable
 reverse tree =
     case tree of
-    Empty -> Empty
-    Branch x l r -> Branch x (reverse r) (reverse l)
+        Empty ->
+            Empty
+
+        Branch x l r ->
+            Branch x (reverse r) (reverse l)
+
 
 addReverse : comparable -> Tree comparable -> Tree comparable
 addReverse val tree =
     Branch val (reverse tree) tree
 
+
 solve : comparable -> Int -> List (Tree comparable)
 solve val n =
     if Basics.modBy 2 n == 0 then
         []
+
     else
-        P55.solve val (n // 2) |>
-        List.map (addReverse val)
+        P55.solve val (n // 2)
+            |> List.map (addReverse val)
